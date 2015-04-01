77da22517aabf3ad6822f5858f5ee01ee5a7e167
Phil Monroe <phil@philmonroe.com>
2015-04-01 14:21:38 -0500

    configuring newrelic

--- lib/rbsavvy_commons.rb
+++ lib/rbsavvy_commons.rb
@@ -1,19 +1,31 @@
-require 'unicorn'
-require 'rack/handler/unicorn'
-require 'lograge'
-require 'rails_serve_static_assets'
-require 'newrelic_rpm'
-require 'rollbar'
-require 'dotenv'
-
-Dotenv.load if Rails.env.test? or Rails.env.development?
-
 module RBSavvy
   def self.logger
     @@logger ||= ActiveSupport::TaggedLogging.new(RBSavvy::Logger.new)
   end
+
+  def self.file_path(rel_path)
+    path = File.expand_path("../#{rel_path}", __FILE__)
+    path if File.exists?(path)
+  end
 end
 
+# Dotenv
+require 'dotenv'
+Dotenv.load if Rails.env.test? or Rails.env.development?
 
+# New Relic
+ENV['NRCONFIG'] ||= RBSavvy.file_path("config/newrelic.yml")
+require 'newrelic_rpm'
+
+# Unicorn
+require 'unicorn'
+require 'rack/handler/unicorn'
+
+# Random
+require 'lograge'
+require 'rails_serve_static_assets'
+require 'rollbar'
+
+# rbsavvy specific
 require 'rbsavvy/logger'
-require 'rbsavvy/engine'
+require 'rbsavvy/railtie'
