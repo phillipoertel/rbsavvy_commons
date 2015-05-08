94d932fe45a059e714dfde17487881264e9d5feb
Phil Monroe <phil@philmonroe.com>
2015-05-07 22:47:51 -0700

    log session id

--- lib/rbsavvy/railtie.rb
+++ lib/rbsavvy/railtie.rb
@@ -4,7 +4,13 @@ module RBSavvy
       Rails.logger = app.config.logger = RBSavvy.logger
       config.log_level = RBSavvy::Logger::LOG_LEVEL
 
+      app.config.middleware.delete(ActionDispatch::Cookies)
+      app.config.middleware.delete(ActionDispatch::Session::CookieStore)
+      app.config.middleware.insert_before(Rails::Rack::Logger, ActionDispatch::Session::CookieStore)
+      app.config.middleware.insert_before(ActionDispatch::Session::CookieStore, ActionDispatch::Cookies)
+
       app.config.log_tags = [
+        lambda { |req| req.session.id || "-" },
         :uuid
       ]
 
