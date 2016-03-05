16daa93e3f939e738973cd7894d2df2f38177b32
Ricky Chilcott <ricky@rbsavvy.com>
2016-03-05 17:01:29 -0500

    Don't use Middleware delete operation in Rails 5

--- lib/rbsavvy/railtie.rb
+++ lib/rbsavvy/railtie.rb
@@ -5,8 +5,11 @@ module RBSavvy
       Rails.logger = app.config.logger = RBSavvy.logger
       config.log_level = RBSavvy::Logger::LOG_LEVEL
 
-      app.config.middleware.delete(ActionDispatch::Cookies)
-      app.config.middleware.delete(ActionDispatch::Session::CookieStore)
+      if Rails::VERSION::MAJOR < 5
+        app.config.middleware.delete(ActionDispatch::Cookies)
+        app.config.middleware.delete(ActionDispatch::Session::CookieStore)
+      end
+
       app.config.middleware.insert_before(Rails::Rack::Logger, ActionDispatch::Session::CookieStore)
       app.config.middleware.insert_before(ActionDispatch::Session::CookieStore, ActionDispatch::Cookies)
 
